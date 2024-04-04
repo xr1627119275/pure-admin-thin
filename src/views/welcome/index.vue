69cda8166e931424be33996cda12b04d5fb2b020
xurui <Xr@13275708870>
2024-04-04 23:27:48 +0800

    chore: 添加地区选择

--- src/views/welcome/index.vue
+++ src/views/welcome/index.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
+import { pcaTextArr } from "element-china-area-data";
+import { ref } from "vue";
 defineOptions({
   name: "Welcome"
 });
+import { RegionSelects } from "v-region";
 </script>
 
 <template>
-  <h1>Pure-Admin-Thin（非国际化版本）</h1>
+  <h1>地区选择</h1>
+  <RegionSelects />
 </template>
